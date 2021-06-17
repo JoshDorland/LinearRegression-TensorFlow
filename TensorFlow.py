3ba5c6dcb6afad82d4dd351a08a04a0683a96e1f
JoshDorland <85595406+JoshDorland@users.noreply.github.com>
2021-06-17 09:07:12 -0400

    Add files via upload

---
+++ TensorFlow.py
@@ -0,0 +1,37 @@
+import tensorflow
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn import linear_model
+from sklearn.utils import shuffle
+
+# Since our data is seperated by semicolons we need to do sep=";"
+data = pd.read_csv("student-mat.csv", sep=";")
+
+data = data[["G1", "G2", "G3", "studytime", "failures", "absences", "Dalc", "Walc", "goout"]]
+
+predict = "G3" #Final grades (Label = what you are trying to predict)
+
+#This will remove predict of G3 from the array
+x = np.array(data.drop([predict], 1)) #Features/attributes
+
+y = np.array(data[predict]) #Labels
+
+
+#Splits into 4 differnt arrays
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
+
+linear = linear_model.LinearRegression()
+
+linear.fit(x_train, y_train)
+
+acc = linear.score(x_test, y_test)
+print(acc)
+
+print("Co: \n", linear.coef_)
+print("Intercept: \n", linear.intercept_)
+
+predictions = linear.predict(x_test)
+
+for x in range(len(predictions)):
+    print(predictions[x], x_test[x], y_test[x])
